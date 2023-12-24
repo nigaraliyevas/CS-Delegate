59454daf9afe6483d2ec92b4b15efe3ec17b9665
nigaraliyevas <naliyeva965@gmail.com>
2023-12-25 02:37:38 +0400

    initial

---
+++ DelegateClass.cs
@@ -0,0 +1,5 @@
+﻿namespace Delegates
+{
+    public delegate bool Check(int n,int m);
+    public delegate void Print(string word1);
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Delegates;
+/*Console.WriteLine(Sum(IsEven,5,6,8,11,13));
+bool IsEven(int num, int m)
+{
+    return num % 2 == 0;
+}
+bool IsOdd(int num)
+{
+    return num % 2 == 1;
+}
+int Sum(Check method,params int[] nums)
+{
+    int sum = 0;
+    foreach (var item in nums)
+    {
+        if (method(item))
+        {
+            sum += item;
+        }
+    }
+    return sum;
+}*/
+Print print = Info;
+print += StrLength;
+print("lorem");
+//void Info(string str1,string str2)
+//{
+//    Console.WriteLine($"{str1} {str2}");
+//}
+void Info(string str1)
+{
+    Console.WriteLine(str1);
+}
+void StrLength(string str1)
+{
+    Console.WriteLine(str1.Length);
+}
